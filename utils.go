f966b7f582be99a04b631f5bdadddd901c666793
André Sousa <andreosousa95@gmail.com>
2022-09-27 20:35:45 +0000

    Added simple logging method

--- utils.go
+++ utils.go
@@ -8,14 +8,15 @@ import (
 	"time"
 )
 
-/*FindStringInSlice detects if a string is in a slice.
+/*
+FindStringInSlice detects if a string is in a slice.
 
 Receives:
-	* str (string) - String that will be searched inside the slice
-	* sl ([]string) - Slice of strings
+  - str (string) - String that will be searched inside the slice
+  - sl ([]string) - Slice of strings
 
 Returns:
-	* bool - Whether or not the string was found inside the slice
+  - bool - Whether or not the string was found inside the slice
 */
 func FindStringInSlice(str string, sl []string) bool {
 	for _, el := range sl {
@@ -40,12 +41,13 @@ func IsEmpty(field string) bool {
 	return field == ""
 }
 
-/*SetResponse sets the response to be sent to the user in any API endpoints.
+/*
+SetResponse sets the response to be sent to the user in any API endpoints.
 
 Receives:
-	* w (http.ResponseWriter) - Go's HTTP ResponseWriter struct
- 	* statusCode (int) - HTTP status code of the response
- 	* body (HTTPResponse) - Body of the HTTP response using a custom struct
+  - w (http.ResponseWriter) - Go's HTTP ResponseWriter struct
+  - statusCode (int) - HTTP status code of the response
+  - body (HTTPResponse) - Body of the HTTP response using a custom struct
 */
 func SetResponse(w http.ResponseWriter, statusCode int, body HTTPResponse) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
@@ -54,16 +56,16 @@ func SetResponse(w http.ResponseWriter, statusCode int, body HTTPResponse) {
 	return
 }
 
-/*CalculateStringDistance calculates the number of empty characters required to match a
+/*
+CalculateStringDistance calculates the number of empty characters required to match a
 longer string above it (i.e. in a printed table)
 
 Receives:
-	* baseDistance (int) - Length of the string to match
-	* name (string) - String to add empty characters to
+  - baseDistance (int) - Length of the string to match
+  - name (string) - String to add empty characters to
 
 Returns:
-	* string - New string with the required added spaces to match the string above it
-
+  - string - New string with the required added spaces to match the string above it
 */
 func CalculateStringDistance(baseDistance int, name string) string {
 	stringDistanceLen := baseDistance - len(name)
@@ -80,4 +82,9 @@ func CalculateStringDistance(baseDistance int, name string) string {
 func HandleError(err error) {
 	fmt.Printf("%s", err)
 	os.Exit(1)
+}
+
+// LogMessage simply logs a message to the console with the current date/time.
+func LogMessage(message string) {
+	fmt.Printf("[%s] %s", GetCurrentDateTime(), message)
 }
